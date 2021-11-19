82c20e58a40b125e1840c3263f99ef8a25006aaf
Sudeep <sudeepm117@gmail.com>
2021-11-19 15:32:56 +0530

    DB connect

---
+++ src/db/mongoose.js
@@ -0,0 +1,18 @@
+/*   File contains mongodb connection */
+
+//import mongoose package
+const mongoose = require("mongoose");
+
+( async () => {
+  try{
+    await mongoose.connect(process.env.MONGODB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Connected to db");
+  }
+  catch(err) {
+    console.log(err);
+  }
+}) ()
+

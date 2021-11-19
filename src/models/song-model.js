0550831ea122c3f2e21c16e4808d5548b526fb9e
Ruturaj Patil <ruturajp551@gmail.com>
2021-11-19 22:56:24 +0530

    Song model updated

--- src/models/song-model.js
+++ src/models/song-model.js
@@ -1,43 +1,35 @@
-const { Decimal128 } = require('bson');
-const mongoose = require('mongoose');
+const { Decimal128 } = require("bson");
+const mongoose = require("mongoose");
 
 const schema = mongoose.Schema;
 
 const songSchema = new schema({
-    AlbumID : {
-        // type : Number,
-        type : String,
-        required : true
-    },
-    AlbumName : {
-        type : String,
-        required : true
-    },
-    ArtistName : {
-        type : String,
-        required : true
-    },
-    Duration : {
-        // type : Decimal128,
-        type : String,
-        required : true
-    },
-    Title : {
-        type : String,
-        required : true
-    },
-    Year : {
-        // type : Number,
-        type : String,
-        required : true
-    }
-    // },
-    // Category : {
-    //     type : String,
-    //     required : true
-    // }
+  Title: {
+    type: String,
+    required: true,
+  },
+  Artist: {
+    type: String,
+    required: true,
+  },
+  Genre: {
+    type: String,
+    required: true,
+  },
+  Year: {
+    type: Number,
+    required: true,
+  },
+  Duration: {
+    type: Number,
+    required: true,
+  },
+  Popularity: {
+    type: Number,
+    required: true,
+  },
 });
 
-const Song = mongoose.model('Song', songSchema, 'Songs'); // only checks lower case pluralized names
+const Song = mongoose.model("Song", songSchema, "Songs"); // only checks lower case pluralized names
 
 module.exports = Song;

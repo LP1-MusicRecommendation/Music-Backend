bc460761fb0de91198458fecef77fbc3deb835b7
Ruturaj Patil <ruturajp551@gmail.com>
2021-11-20 11:55:55 +0530

    Remove unused packages

--- src/models/user-model.js
+++ src/models/user-model.js
@@ -43,10 +43,8 @@ const userSchema = new schema({
 });
 
 userSchema.methods.generateAuthToken = async function () {
-  console.log("Hello");
   const user = this;
   const token = jwt.sign({ _id: user._id.toString() }, process.env.JWT_SECRET);
-
   user.tokens = user.tokens.concat({ token });
   await user.save();
 
